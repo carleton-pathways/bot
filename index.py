a338c259c4918ec77d7f83e0dbf648c1400edc5e
Reeto G <reetog29@gmail.com>
2023-04-29 23:06:52 -0400

    updated the course scraper to properly index for term, coursecode, course number and other relevant information

--- index.py
+++ index.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import Select
-
 from selenium.webdriver.common.by import By
 
 
@@ -11,20 +9,24 @@ def main():
     options = Options()
     options.add_experimental_option("detach", True)
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options = options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.get("https://central.carleton.ca/prod/bwysched.p_display_course?wsea_code=EXT&term_code=202230&disp=18299494&crn=34056")
     driver.maximize_window()
 
-    b_element = driver.find_element(By.XPATH, "//b[contains(text(), 'target text')]")
+    term = driver.find_element(By.XPATH, "//td[contains(., 'Registration Term:')]//following-sibling::td")
+    courseCode = driver.find_element(By.XPATH, "//td[contains(., 'Subject:')]//following-sibling::td")
+    CRN = driver.find_element(By.XPATH, "//td[contains(., 'CRN:')]//following-sibling::td")
+    title = driver.find_element(By.XPATH, "//td[contains(., 'Title:')]//following-sibling::td")
+    description = driver.find_element(By.XPATH, "//td[contains(., 'Course Description:')]//following-sibling::td")
+    credit = driver.find_element(By.XPATH, "//td[contains(., 'Course Credit Value:')]//following-sibling::td")
 
-    # 2. Go backwards to the <tr> element
-    tr_element = b_element.find_element(By.XPATH, "./sibling::tr")
+    print(term.text)
+    print(courseCode.text)
+    print(CRN.text)
+    print(title.text)
+    print(description.text)
+    print(credit.text)
 
-    # 3. Print the other <td> elements inside the <tr>
-    td_elements = tr_element.find_elements(By.XPATH,"./td")
-    for td in td_elements:
-        if td != b_element:
-            print(td.text)
 
 if __name__ == "__main__":
     main()
