0131ce687b16f6797abe4c429d22d0fa433a4be5
Iman1121 <imanullah1112@gmail.com>
2023-05-01 22:31:40 -0400

    created parse

--- dataParser.py
+++ dataParser.py
@@ -1,15 +1,15 @@
-class DataParser: 
+class DataParser:
 
-    #takes in a string of the course title and return a dictionary
+    # takes in a string of the course title and return a dictionary
     def parse_section_information(self, section_information=""):
-        
+
         # Split into list of words
         words = section_information.split()
 
         # If there is no list of words, meaning empty string was passed in
         if (not words):
             raise ValueError("Empty string passed")
-        
+
         # If there is only one word, meaning either not the full string was passed in or the string had no
         if (len(words) <= 1):
             raise ValueError("No spaces in string passed")
@@ -29,8 +29,11 @@ class DataParser:
 
         # If section or tut_id not set, set them to None
         if ("section" not in values):
-            values["section"] = None 
+            values["section"] = None
         if ("tut_id" not in values):
-            values["tut_id"] = None               
-            
+            values["tut_id"] = None
+
         return values
+
+    def parse1():
+        return 0
