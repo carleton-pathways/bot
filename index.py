cb9d1f4b90f67bcbff0cc9c9e8ec70fecf9809d0
Kunwar Nir <knir45@gmail.com>
2023-05-20 12:39:33 -0400

    storing all variables in dict

--- index.py
+++ index.py
@@ -21,31 +21,18 @@ def main():
     info_dict = {}
 
     term_array = ['Registration Term', 'Subject', 'CRN', 'Title', 'Course Description', 'Course Credit Value', 'Schedule Type', 'Status', 'Section Information', 'Year in Program',
-                  'Level Restriction', 'Degree Restriction', 'Major Restriction', 'Program Restrictions', 'Department Restriction', 'Faculty Restriction',]
+                  'Level Restriction', 'Degree Restriction', 'Major Restriction', 'Program Restrictions', 'Department Restriction', 'Faculty Restriction', 'Meeting Date', 'Days', 
+                  'Time', 'Building', 'Room', 'Schedule', 'Instructor',]
     
     # finds everything in first table
-    for term in term_array:
-        info_dict[(term.replace(" ", "_")).lower()] = driver.find_element(By.XPATH, "//td[contains(., '%s:')]//following-sibling::td"%(term)).text
+    for i in range(0, 15):
+        info_dict[(term_array[i].replace(" ", "_")).lower()] = driver.find_element(By.XPATH, "//td[contains(., '%s:')]//following-sibling::td"%(term_array[i])).text
 
     # finds everything in second table
     for i, col in enumerate(secondtable.find_all("tr")[1].find_all("td")):
-        if (i == 0):
-            info_dict["meeting_date"] = col.text
-        elif (i == 1):
-            info_dict["days"] = col.text
-        elif (i == 2):
-            info_dict["time"] = col.text
-        elif (i == 3):
-            info_dict["building"] = col.text
-        elif (i == 4):
-            info_dict["room"] = col.text
-        elif (i == 5):
-            info_dict["schedule"] = col.text
-        elif (i == 6):
-            info_dict["instructor"] = col.text
+        info_dict[(term_array[i+16].replace(" ", "_")).lower()] = col.text
 
-
-    print(info_dict)
+    # print(info_dict)
 
 if __name__ == "__main__":
     main()
